2c57691ed5a397a2d959d5f964e20095ca344a02
taofeek-ojewande <taofeekojewande@nda.edu.ng>
2024-01-16 19:33:41 +0100

    comments removed

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def uppercase(str):
-    # function that prints a string in uppercase followed by a new line
     for i in str:
         value = ord(i)
         if value >= 97 and value <= 122:
