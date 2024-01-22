32777e4274d1aee5f26a17df1de52668ac140801
taofeek-ojewande <taofeekojewande@nda.edu.ng>
2024-01-22 23:02:08 +0100

    task 2 - 1 argument considered

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 from sys import argv
-if len(argv) == 1:
-    print("0 arguments.")
-else:
-    args_num = len(argv) - 1
-    print("{} arguments:".format(args_num))
-    for args_index in range(1, len(argv)):
-        print("{}: {}".format(args_index, argv[args_index]))
+if __name__ == "__main__":
+    if len(argv) == 1:
+        print("0 arguments.")
+    else:
+        args_num = len(argv) - 1
+        if args_num == 1:
+            print("{} argument:".format(args_num))
+        else:
+            print("{} arguments:".format(args_num))
+        for args_index in range(1, len(argv)):
+            print("{}: {}".format(args_index, argv[args_index]))
 """ args_index = 1
     while args_num > 0:
     print("{}: {}".format(args_num, argv(args_index)))
