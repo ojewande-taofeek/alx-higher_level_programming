9fcf52798f54138bfbffbb88495a553691cb2e5d
taofeek-ojewande <taofeekojewande@nda.edu.ng>
2024-01-22 22:02:04 +0100

    task 1

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+
+if __name__ == "__main__":
+    """ program that imports functions from the file calculator_1.py,
+        does some Maths, and prints the result
+    """
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
