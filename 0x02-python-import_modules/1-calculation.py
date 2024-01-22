9a245725f735172acf290fe6064c5f26a1ab988a
taofeek-ojewande <taofeekojewande@nda.edu.ng>
2024-01-22 22:12:20 +0100

    task 1 updated

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-
 if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
     """ program that imports functions from the file calculator_1.py,
         does some Maths, and writes the result
-    """
+   """
+
     a = 10
     b = 5
-    print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a, b, sub(a, b)))
-    print("{} * {} = {}".format(a, b, mul(a, b)))
-    print("{} / {} = {}".format(a, b, div(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
