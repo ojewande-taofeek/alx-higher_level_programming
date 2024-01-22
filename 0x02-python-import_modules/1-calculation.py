8ce5b44c2058c29116e78e6eb5bb1006c0cc12d8
taofeek-ojewande <taofeekojewande@nda.edu.ng>
2024-01-22 22:26:20 +0100

    task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
+
     """ program that imports functions from the file calculator_1.py,
         does some Maths, and writes the result
    """
-
     a = 10
     b = 5
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
