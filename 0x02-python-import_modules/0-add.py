a24181fdb78cd8fd891ef5e2fc549e36a09cecca
taofeek-ojewande <taofeekojewande@nda.edu.ng>
2024-01-22 21:44:58 +0100

    task 0

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from add_0 import add
+    # program that imports the function def add(a, b)
+
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
