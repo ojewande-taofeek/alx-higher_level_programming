0f5048cbe157aa0d070d4f744f3aaf707f1607a0
taofeek-ojewande <taofeekojewande@nda.edu.ng>
2024-01-25 15:40:22 +0100

    task 7

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    # function that adds 2 tuples.
+    new_tuple = ()
+    if len(tuple_a) == 1:
+        tuple_a = tuple_a[0], 0
+    elif len(tuple_b) == 1:
+        tuple_b = tuple_b[0], 0
+    elif len(tuple_a) == 0:
+        tuple_a = 0, 0
+    elif len(tuple_b) == 0:
+        tuple_b = 0, 0
+    new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return new_tuple
