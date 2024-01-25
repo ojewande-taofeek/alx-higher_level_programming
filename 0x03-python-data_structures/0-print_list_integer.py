b1c572f2dc5e21b0ed5c93d37bc731b40b59d0bb
taofeek-ojewande <taofeekojewande@nda.edu.ng>
2024-01-25 07:28:58 +0100

    task 0

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    # function that prints all integers of a list
+    if my_list:
+        for num in my_list:
+            print("{:d}".format(num))
