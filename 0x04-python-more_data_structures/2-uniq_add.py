cbe29082ea8698ac5ff837708896f90eb643e16d
taofeek-ojewande <taofeekojewande@nda.edu.ng>
2024-01-29 11:55:13 +0100

    task 2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,8 +3,8 @@ def uniq_add(my_list=[]):
     """ function that adds all unique integers
         in a list (only once for each integer)
     """
+    add = 0
     if my_list:
-        add = 0
         for x in set(my_list):
             add += x
     return add
