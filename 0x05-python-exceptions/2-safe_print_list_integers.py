a4b21edb73d038317e8a76f03b8e57bf8200c83e
taofeek-ojewande <taofeekojewande@nda.edu.ng>
2024-02-05 12:33:38 +0100

    task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    # function that prints the first x elements of a list and only integers
+    counter = 0
+    if my_list:
+        for mem in my_list:
+            try:
+                if counter < x:
+                    print("{:d}".format(mem), end="")
+                    counter += 1
+            except (TypeError, ValueError, IndexError):
+                pass
+    print()
+    return counter
