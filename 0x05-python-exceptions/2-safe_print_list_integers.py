c30ee9e5f258ac570e6fdfbbfa60d3b7bb6e0447
taofeek-ojewande <taofeekojewande@nda.edu.ng>
2024-02-05 12:39:55 +0100

    always a list

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,13 +2,12 @@
 def safe_print_list_integers(my_list=[], x=0):
     # function that prints the first x elements of a list and only integers
     counter = 0
-    if my_list:
-        for mem in my_list:
-            try:
-                if counter < x:
-                    print("{:d}".format(mem), end="")
-                    counter += 1
-            except Exception:
-                pass
+    for mem in my_list:
+        try:
+            if counter < x:
+                print("{:d}".format(mem), end="")
+                counter += 1
+        except Exception:
+            pass
     print()
     return counter
