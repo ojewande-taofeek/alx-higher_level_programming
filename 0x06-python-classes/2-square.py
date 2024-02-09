a41608a3564950cfb549324bf27cfed700c1fb96
taofeek-ojewande <taofeekojewande@nda.edu.ng>
2024-02-09 21:31:33 +0100

    task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""A module that defines defines a square by: (based on 1-square.py) """
+
+
+class Square:
+    """class Square that defines a square"""
+
+    def __init__(self, size=0):
+        """The initialization method
+            Args:
+                size: The size of a sqaure
+            Return:
+                Nothing
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
