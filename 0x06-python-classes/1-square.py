617d1a5bec20be24e9a665fdaae6c17b4ccd9140
taofeek-ojewande <taofeekojewande@nda.edu.ng>
2024-02-09 18:21:04 +0100

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""A module that defines defines a square by: (based on 0-square.py) """
+
+
+class Square:
+    """class Square that defines a square"""
+
+    def __init__(self, size):
+        """The initialization method
+            Attr:
+                size: The size of a sqaure
+            Return:
+                Nothing
+        """
+        self.__size = size
