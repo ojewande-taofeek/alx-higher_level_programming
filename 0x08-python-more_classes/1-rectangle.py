026ac8208ec4652e59f4930daad5ea09b488d4ba
taofeek-ojewande <taofeekojewande@nda.edu.ng>
2024-02-12 22:20:44 +0100

    task 1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""The module defines class Rectangle based on 0-rectangle.py"""
+
+
+class Rectangle:
+    """Class Rectangle that defines a rectangle"""
+    def __init__(self, width=0, height=0):
+        """Instantiation with optional width and height"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """The getter for width"""
+        return self.__width
+
+    @property
+    def height(self):
+        """The getter for height"""
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """The setter for width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """The setter for height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
