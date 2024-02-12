3d12dc0382587596f0cee52f4934e520b34644fd
taofeek-ojewande <taofeekojewande@nda.edu.ng>
2024-02-12 23:43:52 +0100

    space added

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -58,5 +58,5 @@ class Rectangle:
         """return a string representation of the rectangle to be
            able to recreate a new instance by using eval()
         """
-        return ("{}({:d},{:d})".format(self.__class__.__name__,
+        return ("{}({:d}, {:d})".format(self.__class__.__name__,
                 self.__width, self.__height))
