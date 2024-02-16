9a1b74d18071fd0a258c7874055eec97ecba76b8
taofeek-ojewande <taofeekojewande@nda.edu.ng>
2024-02-16 22:03:15 +0100

    task 5

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """The class for the max_integer unit testing"""
+    def test_values(self):
+        """Test if the arg is an instance of list"""
+        my_list = [1, 2, 5, 3, 4]
+        self.assertEqual(5, max_integer(my_list))
+        self.assertEqual(None, max_integer())
+
+    def test_instance(self):
+        """Test if the arg is an instance of list"""
+        my_list = [1, 2, 5, 3, 4]
+        self.assertIsInstance(my_list, list)
+        new_list = "try"
+        self.assertNotIsInstance(new_list, list)
+
+    def test_not_an_int(self):
+        """Element not an integer"""
+        my_list = [1, 2, 5, "three"]
+        with self.assertRaises(TypeError):
+            max_integer(my_list)
