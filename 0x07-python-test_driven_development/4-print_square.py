b4411fea83812fa87eb0a5ce1d69e7407fd32542
taofeek-ojewande <taofeekojewande@nda.edu.ng>
2024-02-16 07:55:34 +0100

    task 3

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Contains a function that prints a square with the character #"""
+
+
+def print_square(size):
+    """
+        function that prints a square with the character #
+        Args:
+            size: size length of the square (int)
+        Returns:
+            Nothing
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if isinstance(size, float) and size <=0:
+        raise TypeError("size must be an integer")
+    for n in range(size):
+        print("#" * size)
