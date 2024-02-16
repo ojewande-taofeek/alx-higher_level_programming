84054e2094f0d9656efee8ac6f2ec34960a1c1fa
taofeek-ojewande <taofeekojewande@nda.edu.ng>
2024-02-16 22:14:34 +0100

    more tests considered

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -11,6 +11,14 @@ class TestMaxInteger(unittest.TestCase):
         """Test if the arg is an instance of list"""
         my_list = [1, 2, 5, 3, 4]
         self.assertEqual(5, max_integer(my_list))
+        my_list = [5, 2, 1, 3, 4]
+        self.assertEqual(5, max_integer(my_list))
+        my_list = [1, 2, 4, 3, 5]
+        self.assertEqual(5, max_integer(my_list))
+        my_list = [1, 2, 5, -3, 4]
+        self.assertEqual(5, max_integer(my_list))
+        my_list = [-1, -2, -5, -3, -4]
+        self.assertEqual(-1, max_integer(my_list))
         self.assertEqual(None, max_integer())
 
     def test_instance(self):
