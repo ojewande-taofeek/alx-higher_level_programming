ac07b48d79dbff603a9a319ff801b05cc2dccb38
taofeek-ojewande <taofeekojewande@nda.edu.ng>
2024-02-16 17:41:30 +0100

    task 5

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Contains a function that prints a text with 2 new lines
+    after each of these characters: ., ? and :
+"""
+
+
+def text_indentation(text):
+    """
+        function that prints a text with 2 new lines after
+        each of these characters: ., ? and :
+        Args:
+            text(str): string to be updated
+        Returns:
+            Nothing
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    text = text.strip()
+    pun = None
+    for word in text:
+        if word == "." or word == "?" or word == ":":
+            pun = word
+            print("{}\n".format(word))
+        elif pun:
+            pun = None
+            continue
+        else:
+            print(word, end="")
