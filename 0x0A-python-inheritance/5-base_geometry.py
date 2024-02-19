2e4687935faf2432f31f09584891de38db871b93
taofeek-ojewande <taofeekojewande@nda.edu.ng>
2024-02-19 22:30:38 +0100

    task 5

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+    Write an empty class BaseGeometry.
+
+    You are not allowed to import any module
+"""
+
+
+class BaseGeometry:
+    """An empty base class"""
+    pass
