f906e1cd5d1ca14af4bf33bf68b500d6280580b4
taofeek-ojewande <taofeekojewande@nda.edu.ng>
2024-02-19 23:45:59 +0100

    task 8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+"""
+    Write a class Rectangle that inherits from
+    BaseGeometry (7-base_geometry.py)
+    Instantiation with width and height: def __init__(self, width, height):
+    width and height must be private. No getter or setter
+    width and height must be positive integers, validated by integer_validator
+"""
+
+
+class Rectangle(BaseGeometry):
+    """A sub class of BaseGeometry"""
+    def __init__(self, width, height):
+        """Constructor for class rectangle"""
+        super().integer_validator("width", width)
+        self.__width = width
+        super().integer_validator("height", height)
+        self.__height = height
