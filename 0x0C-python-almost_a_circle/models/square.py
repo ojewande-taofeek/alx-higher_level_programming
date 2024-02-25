92fa3db72180ca2db73f90ebcc4b6fccd9d5a899
taofeek-ojewande <taofeekojewande@nda.edu.ng>
2024-02-25 10:46:22 +0100

    square class methods and tests added

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,9 +8,72 @@ class Square(Rectangle):
     """A child class of class Rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
         """The class constructor"""
+        self.size = size
         super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """Public instance getter for size attr"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setter for the size attr"""
+        self.width = value
+        self.height = value
 
     def __str__(self):
         """The informal representation of square"""
-        return "[Square]  ({}) {}/{} - {}" \
+        return "[Square] ({}) {}/{} - {}" \
                .format(self.id, self.x, self.y, self.width)
+
+    def update(self, *args, **kwargs):
+        """update method for Square"""
+        for i in range(len(args)):
+            if i == 0:
+                self.id = args[0]
+            elif i == 1:
+                self.size = args[1]
+            elif i == 2:
+                self.x = args[2]
+            elif i == 3:
+                self.y = args[3]
+        for key, value in kwargs.items():
+            if key == "id":
+                try:
+                    if args[0]:
+                        continue
+                except IndexError:
+                    self.id = value
+            elif key == "size":
+                try:
+                    if args[1]:
+                        continue
+                except IndexError:
+                    self.size = value
+            elif key == "x":
+                try:
+                    if args[2]:
+                        continue
+                except IndexError:
+                    self.x = value
+            elif key == "y":
+                try:
+                    if args[3]:
+                        continue
+                except IndexError:
+                    self.y = value
+
+    def to_dictionary(self):
+        """Returns a Square instance dict"""
+        new_dict = dict()
+        for key, value in self.__dict__.items():
+            if key.endswith("width"):
+                new_dict["size"] = value
+            elif key.endswith("id"):
+                new_dict["id"] = value
+            elif key.endswith("x"):
+                new_dict["x"] = value
+            elif key.endswith("y"):
+                new_dict["y"] = value
+        return new_dict
--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -1,5 +1,130 @@
 #!/usr/bin/python3
 """The unit tests for Square class"""
+import unittest
 from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
+from io import StringIO
+from contextlib import redirect_stdout
+
+
+class TestSquare(unittest.TestCase):
+    """Unit tests for Square"""
+
+    def test_import(self):
+        """Test if Rectangle class is imported"""
+        squ = Square(4)
+        self.assertTrue(isinstance(squ, Square))
+        self.assertTrue(issubclass(type(squ), Square))
+        self.assertTrue(issubclass(type(squ), Rectangle))
+        self.assertTrue(issubclass(type(squ), Base))
+        self.assertTrue(issubclass(type(squ), object))
+
+    def test_attributes(self):
+        """Test the various attrs of an object of Square"""
+        squ = Square(5)
+        self.assertEqual(squ.width, 5)
+        self.assertEqual(squ.height, 5)
+        self.assertEqual(squ.x, 0)
+        self.assertEqual(squ.y, 0)
+        self.assertEqual(squ.id, 1)
+
+        squ = Square(5, 3, 4, 23)
+        self.assertEqual(squ.width, 5)
+        self.assertEqual(squ.height, 5)
+        self.assertEqual(squ.x, 3)
+        self.assertEqual(squ.y, 4)
+        self.assertEqual(squ.id, 23)
+        self.assertEqual(squ.area(), 25)
+
+    def test_exception_raise(self):
+        """Test for exceptions raised"""
+        with self.assertRaises(ValueError, msg="width must be > 0"):
+            Square(-5, 2, 4, 17)
+        with self.assertRaises(ValueError, msg="x must be > 0"):
+            Square(5, -2, 4, 17)
+        with self.assertRaises(ValueError, msg="y must be > 0"):
+            Square(5, 2, -4, 17)
+        with self.assertRaises(ValueError, msg="width must be > 0"):
+            Square(0, 2, 4, 17)
+        with self.assertRaises(TypeError, msg="width must be an integer"):
+            Square("0", 2, 4, 17)
+        with self.assertRaises(ValueError, msg="x must be an integer"):
+            Square(0, [2], 4, 17)
+        with self.assertRaises(ValueError, msg="y must be an integer"):
+            Square(0, 2, {4}, 17)
+
+    def test_str(self):
+        """Test the informal representation of Square"""
+        squ = Square(5, 3, 4, 23)
+        expected_out = "[Square] (23) 3/4 - 5"
+
+        with StringIO() as buffer, redirect_stdout(buffer):
+            print(squ)
+            output = buffer.getvalue().strip()
+            self.assertEqual(expected_out, output)
+
+    def test_display(self):
+        """Test the display method for Square"""
+        squ = Square(2, 3, 4, 23)
+        expected_output = "\n\n\n\n   ##\n   ##\n"
+
+        with StringIO() as buffer, redirect_stdout(buffer):
+            squ.display()
+            output = buffer.getvalue()
+            self.assertEqual(expected_output, output)
+
+    def test_update(self):
+        """Test the update method for Square"""
+        squ = Square(3, 7, 2, 12)
+
+        squ.update(8)
+        expected = "[Square] (8) 7/2 - 3"
+        with StringIO() as buffer, redirect_stdout(buffer):
+            print(squ)
+            output = buffer.getvalue().strip()
+            self.assertEqual(expected, output)
+
+        squ.update(8, 23, 14, 10)
+        expected = "[Square] (8) 14/10 - 23"
+        with StringIO() as buffer, redirect_stdout(buffer):
+            print(squ)
+            output = buffer.getvalue().strip()
+            self.assertEqual(expected, output)
+
+        squ.update(8, 23, 14, 10, 48)
+        expected = "[Square] (8) 14/10 - 23"
+        with StringIO() as buffer, redirect_stdout(buffer):
+            print(squ)
+            output = buffer.getvalue().strip()
+            self.assertEqual(expected, output)
+
+        squ.update(size=15)
+        expected = "[Square] (8) 14/10 - 15"
+        with StringIO() as buffer, redirect_stdout(buffer):
+            print(squ)
+            output = buffer.getvalue().strip()
+            self.assertEqual(expected, output)
+
+        squ.update(size=13, x=7)
+        expected = "[Square] (8) 7/10 - 13"
+        with StringIO() as buffer, redirect_stdout(buffer):
+            print(squ)
+            output = buffer.getvalue().strip()
+            self.assertEqual(expected, output)
+
+        squ.update(8, 23, id=7, y=1)
+        expected = "[Square] (8) 7/1 - 23"
+        with StringIO() as buffer, redirect_stdout(buffer):
+            print(squ)
+            output = buffer.getvalue().strip()
+            self.assertEqual(expected, output)
+
+    def test_dictionary(self):
+        """Test the to_dictionary method"""
+        rec = Square(2, 3, 7, 21)
+        expected = {'id': 21, 'size': 2, 'x': 3, 'y': 7}
+        output = rec.to_dictionary()
+        self.assertDictEqual(expected, output)
+
+        self.assertTrue(issubclass(type(output), dict))
