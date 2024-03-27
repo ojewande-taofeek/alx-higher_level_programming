d1e555063fc3489aeb037f6dc33fd19e6cd8ebef
taofeek-ojewande <taofeekojewande@nda.edu.ng>
2024-03-27 16:24:27 +0100

    JOIN keyword removed from comments

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -3,7 +3,6 @@
 -- The states table contains only one record where
 -- name = California (but the id can be different, as per the example)
 -- Results must be sorted in ascending order by cities.id
--- You are not allowed to use the JOIN keyword
 -- The database name will be passed as an argument of the mysql command
 SELECT id, name FROM cities WHERE state_id=(
     SELECT id from states WHERE name="California"
