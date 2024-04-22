9357b17671e12bdad955f2659344ae405e1e4930
taofeek-ojewande <taofeekojewande@nda.edu.ng>
2024-04-22 23:02:45 +0100

    task 10 - incomplete

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+//  script that computes and prints a factorial
+const { argv } = require('node:process');
+function factorial (num) {
+  if (Number(num)) {
+    if (num === 0 || num === 1) {
+      return (1);
+    }
+    return (num * factorial(num - 1));
+  }
+}
+const fact = factorial(argv[2]);
+console.log(fact);
