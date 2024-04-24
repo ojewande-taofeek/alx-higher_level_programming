3a3ded754411cfe482e68ad8f65fb6c568fd98f8
taofeek-ojewande <taofeekojewande@nda.edu.ng>
2024-04-24 16:19:02 +0100

    inherit from 5-square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,12 +2,8 @@
 // class Square that defines a square and inherits from Rectangle of 4-rectangle.js
 // Create an instance method called charPrint(c) that prints the rectangle using the character c
 // If c is undefined, use the character X
-const Rectangle = require('./4-rectangle');
-module.exports = class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+const Square1 = require('./5-square');
+module.exports = class Square extends Square1 {
   charPrint (c) {
     if (c === undefined) {
       this.print();
