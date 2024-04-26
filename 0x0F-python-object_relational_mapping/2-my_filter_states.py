74137410e136c23d7db2eee73a22868c104e2fa9
taofeek-ojewande <taofeekojewande@nda.edu.ng>
2024-04-26 10:04:54 +0100

    format used

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -19,9 +19,9 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3])
     cur = conn.cursor()
-    cur.execute("""SELECT * FROM states WHERE name=%s
+    cur.execute("""SELECT * FROM states WHERE name='{}'
                  ORDER BY id ASC
-                 """, (argv[4],))
+                 """.format(argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)
