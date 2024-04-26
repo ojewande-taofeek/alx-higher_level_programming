2af475ac159729004d153da6fa22a35e22dc7f6e
taofeek-ojewande <taofeekojewande@nda.edu.ng>
2024-04-26 09:25:42 +0100

    not executable when imported considered

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 """ script that lists all states from the database hbtn_0e_0_usa:
+    Your script should take 3 arguments: mysql username
+    mysql password and database name (no argument validation needed)
+    You must use the module MySQLdb (import MySQLdb)
+    Your script should connect to a MySQL server running on
+    localhost at port 3306
+    Results must be sorted in ascending order by states.id
+    Results must be displayed as they are in the example below
+    Your code should not be executed when imported
 """
 import MySQLdb
 from sys import argv
@@ -12,5 +20,9 @@ cur.execute("SELECT * FROM states ORDER BY id ASC")
 rows = cur.fetchall()
 for row in rows:
     print(row)
+
 cur.close()
 conn.close()
+
+if __name__ == "__main__":
+    __import__("0-select_states")
