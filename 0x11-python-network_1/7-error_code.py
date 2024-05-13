ac9570179964ab8ddbca965ddbe02b041521f2c3
taofeek-ojewande <taofeekojewande@nda.edu.ng>
2024-05-13 07:30:13 -0700

    task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+    Python script that takes in a URL, sends a request to the URL and displays
+    the body of the response.
+    If the HTTP status code is greater than or equal to 400, print: Error code:
+    followed by the value of the HTTP status code
+"""
 from sys import argv
 import requests
 from requests.exceptions import HTTPError
