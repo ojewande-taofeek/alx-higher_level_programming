8fe7f6d157c62d559af6ffa884549e2efbb24c91
taofeek-ojewande <taofeekojewande@nda.edu.ng>
2024-05-13 13:45:32 +0100

    error code

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+    Python script that takes in a URL, sends a request to the URL and displays
+    the body of the response (decoded in utf-8).
+"""
+from urllib.request import Request, urlopen
+from urllib.error import HTTPError
+from sys import argv
+
+
+if __name__ == "__main__":
+    try:
+        req = Request(argv[1])
+        with urlopen(req) as the_req:
+            html = the_req.read()
+    except HTTPError as e:
+        print("Error code: {}".format(e.code))
