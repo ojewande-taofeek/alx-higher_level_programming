69742d07da69f18dd71222519f9035776ebcc66d
taofeek-ojewande <taofeekojewande@nda.edu.ng>
2024-05-13 12:31:26 +0100

    all codes moved to if body

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """
-    Python script that takes in a URL, sends a request to the URL and
-    displays the value of the X-Request-Id variable found in the header
-    of the response.
+    Takes in a URL, sends a request to the URL and displays the value of the
+    X-Request-Id variable found in the header of the response.
 """
 from urllib.request import Request, urlopen
 from sys import argv
@@ -12,4 +11,4 @@ if __name__ == "__main__":
     req = Request(argv[1])
     with urlopen(req) as the_res:
         html = the_res.info()
-print(html.get("X-Request-Id"))
+    print(html.get("X-Request-Id"))
