aa8b8a03f9dcc43e39ef1531ecd9067623989fc8
taofeek-ojewande <taofeekojewande@nda.edu.ng>
2024-05-13 10:20:39 -0700

    task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+    Python script that takes in a letter and sends a POST request
+    to http://0.0.0.0:5000/search_user with the letter as a parameter
+"""
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    q = ""
+    if len(argv) > 1:
+        if not argv[1].isdigit():
+            q = argv[1]
+    payload = {'q': q}
+    try:
+        req = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+        new_json = req.json()
+        if new_json:
+            print("[{}] {}".format(new_json.get("id"), new_json.get("name")))
+        else:
+            print("No result")
+    except requests.exceptions.JSONDecodeError:
+        print("Not a valid JSON")
