69fa6e618a634bb759e3eb7067a5107ec3fb8808
taofeek-ojewande <taofeekojewande@nda.edu.ng>
2024-05-25 16:11:22 +0100

    task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+     lists all states with a name starting with N (upper N)
+     from the database hbtn_0e_0_usa
+"""
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3])
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    all_N_states = cur.fetchall()
+    for n_states in all_N_states:
+        print(n_states)
+    cur.close()
+    conn.close()
