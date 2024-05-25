9807cda1c7c11e462dbf03a233c4d0c9557448c7
taofeek-ojewande <taofeekojewande@nda.edu.ng>
2024-05-25 18:22:40 +0100

    task 5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+     takes in the name of a state as an argument and
+     lists all cities of that state, using the database hbtn_0e_4_usa
+     SQL injection free
+"""
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3])
+    cur = conn.cursor()
+    if len(argv[4].split()) > 2:
+        exit()
+    cur.execute("SELECT name FROM cities \
+                 WHERE state_id=(SELECT id FROM states \
+                 WHERE BINARY name LIKE '{}')".format(argv[4]))
+    selected_cities = cur.fetchall()
+    counter = cur.rowcount
+    for city in selected_cities:
+        city = city[0]
+        if counter == 1:
+            print("{}".format(city))
+        else:
+            print("{}, ".format(city), end="")
+        counter -= 1
+    cur.close()
+    conn.close()
