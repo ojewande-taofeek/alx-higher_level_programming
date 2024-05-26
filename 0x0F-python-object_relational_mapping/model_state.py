02112e4ca3260bbc67e9d0bce304fdc1ca05ffcc
taofeek-ojewande <taofeekojewande@nda.edu.ng>
2024-05-26 07:31:05 +0100

    task 6 - State class inherited from Base

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    contains the class definition of a State and an
+    instance Base = declarative_base()
+"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+        Inherits from Base
+    """
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
