2501684a7348119732693be71b2940ce046c9381
taofeek-ojewande <taofeekojewande@nda.edu.ng>
2024-06-11 19:45:27 +0100

    (document).ready used

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,10 @@
+/*
+Fetches from https://hellosalut.stefanbohacek.dev/?lang=fr and
+displays the value of hello from that fetch in the HTML tag DIV#hello
+*/
+$(document).ready(function () {
+  $.get('https://hellosalut.stefanbohacek.dev/?lang=fr',
+    function (data) {
+      $('DIV#hello').text(data.hello);
+    });
+});
