af3dabb975aab26a5eff2424c55ace93abd51016
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2025-02-18 10:04:20 +0100

    An Integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/**
+ * script that prints My number: <first argument converted in integer>
+ * if the first argument can be converted to an integer
+ */
+const { argv } = require('node:process');
+if (argv[2] === undefined || isNaN(Number(argv[2]))) {
+  console.log('Not a number');
+} else {
+  console.log(`My number: ${Number(argv[2])}`);
+}
