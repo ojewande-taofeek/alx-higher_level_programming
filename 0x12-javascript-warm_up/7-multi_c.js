bb15d7f64f48344c595696d59e904bf63acb5ed2
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2025-02-18 10:41:26 +0100

    I love C

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const { argv } = require('node:process');
+if (isNaN(argv[2])) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let idx = 0; idx < argv[2]; idx++) {
+    console.log('C is fun');
+  }
+}
