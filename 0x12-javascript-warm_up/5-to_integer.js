4e208ad60d54a0a238c6c2d6b69a5e7908adf832
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2025-02-18 10:11:07 +0100

    An Integer- parseInt and isNaN used

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -7,5 +7,5 @@ const { argv } = require('node:process');
 if (argv[2] === undefined || isNaN(Number(argv[2]))) {
   console.log('Not a number');
 } else {
-  console.log(`My number: ${Number(argv[2])}`);
+  console.log(`My number: ${parseInt(argv[2])}`);
 }
