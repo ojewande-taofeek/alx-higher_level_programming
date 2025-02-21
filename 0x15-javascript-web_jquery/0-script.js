4df8fb6a9f69ea649e58ea5faceb02e4d3230645
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2025-02-21 15:11:48 +0100

    semistandard compliance

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,3 +1,3 @@
 // JavaScript script that updates the text color of the <header> element to red (#FF0000)
-const headerColor = document.querySelector("header");
-headerColor.style.color = "#FF0000";
+const headerColor = document.querySelector('header');
+headerColor.style.color = '#FF0000';
