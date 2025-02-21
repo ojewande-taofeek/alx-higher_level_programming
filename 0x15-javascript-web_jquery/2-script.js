d821dfdea60f305f96ff072eab6ab50936e16224
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2025-02-21 14:48:56 +0100

    Click and turn red - only script used

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,5 +1,6 @@
 // JavaScript script that updates the text color of the <header> 
 // element to red (#FF0000) when the user clicks on the tag DIV#red_header
-const headerColor = (() => {
-    $("div#red_header").css("color", "#FF0000");
+
+$("div#red_header").click(() => {
+    $("#red_header").css("color", "#FF0000");
 })
